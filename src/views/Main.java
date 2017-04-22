4a5f095faae07b448b6bb4b7db1d3b231d7cd048
richardwainwright <richard.wainwright@outlook.com>
2017-04-22 10:52:23 -0400

    initial

---
+++ src/views/Main.java
@@ -0,0 +1,23 @@
+package views;
+
+/**
+ * CMSC 495 - Spring 2017
+ * Professor Dao
+ * Group 2: Richard Wainwright, Dennie Carr, Greg Armstrong, Jik Oh
+ *
+ */
+public class Main {
+
+	/**
+	 * Instantiate the app and open the main application window.
+	 */
+	public static void main(String[] args) {
+	
+		//Run this baby
+		
+		CurrencyView foo = new CurrencyView();
+		foo.setVisible(true);
+
+	}
+
+}
